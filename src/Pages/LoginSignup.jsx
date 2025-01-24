ac8f47f2bb43ce939b776edf0cee50989e906433
Supalak-Anuprai <supalak.anuprai@gmail.com>
2025-01-24 15:25:50 +0700

    Update CSS Login and Register UI

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,28 +1,50 @@
-import { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import './CSS/LoginSignup.css'
-import { ShopContext } from '../Context/ShopContext'
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import "./CSS/LoginSignup.css";
+import { ShopContext } from "../Context/ShopContext";
 
 const LoginSignup = () => {
-
-  const { handleSubmit, formData, handleChange } = useContext(ShopContext)
+  const { handleSubmit, formData, handleChange } = useContext(ShopContext);
 
   return (
-    <div className='loginsignup'>
+    <div className="loginsignup">
       <div className="loginsignup-container">
         <h1>LOGIN</h1>
-        <form onSubmit={handleSubmit} data-type='login'>
-        <div className="loginsignup-fields">
-          <input onChange={handleChange} type="text" name='fname' value={formData.fnae} placeholder='Your Frist Name' />
-          <input onChange={handleChange} type="email" name='email' value={formData.email} placeholder='Email Address' />
-          <input onChange={handleChange} type="password" name='password' value={formData.password} placeholder='Password' />
-        </div>
-        <button type='submit'>Continue</button>
+        <form onSubmit={handleSubmit} data-type="login">
+          <div className="loginsignup-fields">
+            <input
+              onChange={handleChange}
+              type="text"
+              name="fname"
+              value={formData.fnae}
+              placeholder="Your Frist Name"
+            />
+            <input
+              onChange={handleChange}
+              type="email"
+              name="email"
+              value={formData.email}
+              placeholder="Email Address"
+            />
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              value={formData.password}
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit">Continue</button>
+          <p className="loginsignup-login">
+            Apply for an account at{" "}
+            <Link style={{ textDecoration: "none" }} to="/register">
+              <span>Sign Up here</span>
+            </Link>
+          </p>
         </form>
-        <p className="loginsignup-login">Apply for an account at <Link style={{ textDecoration: 'none' }} to='/register'><span>Sign Up here</span></Link></p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
